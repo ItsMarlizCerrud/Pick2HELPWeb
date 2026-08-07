c641bf7a65e5dc79dbe388305d4528b8392cf8a5
MAR004crv <ItsMarlizCerrud>
2026-08-07 15:01:19 -0500

    About cambios

--- js/about.js
+++ js/about.js
@@ -2,60 +2,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const teamSwiper = new Swiper(".teamSwiper", {
 
-        // General
         loop: true,
-        speed: 700,
-        grabCursor: true,
-        centeredSlides: false,
 
-        // Espacio entre tarjetas
+        speed: 800,
+
         spaceBetween: 30,
 
-        // Autoplay
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true
-        },
+        slidesPerView: 4,
 
-        // Flechas
         navigation: {
             nextEl: ".team-next",
             prevEl: ".team-prev",
         },
 
-        // Paginación
         pagination: {
             el: ".team-pagination",
             clickable: true,
         },
 
-        // Responsive
         breakpoints: {
 
             0: {
-                slidesPerView: 1,
-                spaceBetween: 20
-            },
-
-            576: {
-                slidesPerView: 1.2,
-                spaceBetween: 20
+                slidesPerView: 1
             },
 
             768: {
-                slidesPerView: 2,
-                spaceBetween: 25
+                slidesPerView: 2
             },
 
             992: {
-                slidesPerView: 3,
-                spaceBetween: 30
-            },
-
-            1200: {
-                slidesPerView: 4,
-                spaceBetween: 30
+                slidesPerView: 4
             }
 
         }
